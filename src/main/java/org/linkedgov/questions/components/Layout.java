db4b3ed63f2f5d0b19b4587640096f877b8db53a
Luke Wilson-Mawer <lukewm@viscri.co.uk>
2011-10-11 09:32:56 +0100

    Remove chaff from archetype from layout.

--- src/main/java/org/linkedgov/questions/components/Layout.java
+++ src/main/java/org/linkedgov/questions/components/Layout.java
@@ -1,44 +1,37 @@
 package org.linkedgov.questions.components;
 
-import org.apache.tapestry5.*;
-import org.apache.tapestry5.annotations.*;
-import org.apache.tapestry5.ioc.annotations.*;
 import org.apache.tapestry5.BindingConstants;
+import org.apache.tapestry5.Block;
+import org.apache.tapestry5.annotations.Import;
+import org.apache.tapestry5.annotations.Parameter;
+import org.apache.tapestry5.annotations.Property;
 
 /**
- * Layout component for pages of application newapp.
+ * 
+ * Luke Wilson-Mawer <a href="http://viscri.co.uk">Viscri</a> for LinkedGov
+ *
  */
-@IncludeStylesheet("context:layout/layout.css")
+@Import(stylesheet="context:layout/layout.css")
 public class Layout
 {
     /** The page title, for the <title> element and the <h1> element. */
-    @Property
+    @SuppressWarnings("unused")
+	@Property
     @Parameter(required = true, defaultPrefix = BindingConstants.LITERAL)
     private String title;
 
-    @Property
+    @SuppressWarnings("unused")
+	@Property
     private String pageName;
 
-    @Property
+    @SuppressWarnings("unused")
+	@Property
     @Parameter(defaultPrefix = BindingConstants.LITERAL)
     private String sidebarTitle;
 
-    @Property
+    @SuppressWarnings("unused")
+	@Property
     @Parameter(defaultPrefix = BindingConstants.LITERAL)
     private Block sidebar;
 
-    @Inject
-    private ComponentResources resources;
-
-    public String getClassForPageName()
-    {
-      return resources.getPageName().equalsIgnoreCase(pageName)
-             ? "current_page_item"
-             : null;
-    }
-
-    public String[] getPageNames()
-    {
-      return new String[] { "Index", "About", "Contact" };
-    }
 }
