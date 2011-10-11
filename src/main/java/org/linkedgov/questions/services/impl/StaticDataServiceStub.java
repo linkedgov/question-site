cc45e4a8debe55c2fbd1a6c1742ca2dda6797751
Luke Wilson-Mawer <lukewm@viscri.co.uk>
2011-10-11 13:45:59 +0100

    Really shonky stub for static data service

---
+++ src/main/java/org/linkedgov/questions/services/impl/StaticDataServiceStub.java
@@ -0,0 +1,43 @@
+package org.linkedgov.questions.services.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.linkedgov.questions.services.StaticDataService;
+
+public class StaticDataServiceStub implements StaticDataService {
+
+	public List<String> getClasses() {
+		
+		ArrayList<String> classes = new ArrayList<String>();
+		classes.add("http://viscri.co.uk/gpsurgery");
+		classes.add("http://viscri.co.uk/recyclingcentre");
+		classes.add("http://viscri.co.uk/onions");
+		
+		return classes;
+	}
+
+	public List<String> getObjects(String subject, String predicate) {
+		
+		ArrayList<String> objects = new ArrayList<String>();
+		objects.add("50");
+		objects.add("40");
+		objects.add("Nonsense");
+		objects.add("Jonathan Edwards OBE");
+		
+		return objects;
+	}
+
+	public List<String> getPredicates(String subject) {
+		
+		ArrayList<String> predicates = new ArrayList<String>();
+		predicates.add("50");
+		predicates.add("40");
+		predicates.add("Nonsense");
+		predicates.add("Jonathan Edwards OBE");
+		
+		return predicates;
+
+	}
+
+}
