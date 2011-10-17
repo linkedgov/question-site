2d3551dee1884212f13c37e1f4bf2f5cb6b0f8df
Mischa Tuffield <mischa@mmt.me.uk>
2011-10-17 12:37:14 +0100

    Fixed some indentation issue

--- src/test/java/org/linkedgov/questions/model/QueryTest.java
+++ src/test/java/org/linkedgov/questions/model/QueryTest.java
@@ -4,16 +4,15 @@ import junit.framework.TestCase;
 
 public class QueryTest extends TestCase {
 
-	public QueryTest(String name) {
-		super(name);
-	}
+    public QueryTest(String name) {
+        super(name);
+    }
 
-	/** Test the Query to String thing **/
-	public void testThatFeature() throws Exception {
-		Query q = new Query();
-		q.setQuestionType(QuestionType.SELECT);
-
-		assertEquals(q.toSparqlString(), "SELECT DISTINCT ?s ?o WHERE {?s <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> ?o} LIMIT 150");
-	}
+    /** Test the Query to String thing **/
+    public void testThatFeature() throws Exception {
+        Query q = new Query();
+        q.setQuestionType(QuestionType.SELECT);
+        assertEquals(q.toSparqlString(), "SELECT DISTINCT ?s ?o WHERE {?s <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> ?o} LIMIT 150");
+    }
 
 }
