3f68a215e4c1a275ddc54ec04966ddcb352bdc2e
Luke Wilson-Mawer <lukewm@viscri.co.uk>
2011-10-20 14:07:19 +0100

    Remove unused stuff in Layout.java and remove naughtiness in QueryDataServiceImpl accidently committed by me. committed by me

--- src/main/java/org/linkedgov/questions/components/Layout.java
+++ src/main/java/org/linkedgov/questions/components/Layout.java
@@ -1,7 +1,6 @@
 package org.linkedgov.questions.components;
 
 import org.apache.tapestry5.BindingConstants;
-import org.apache.tapestry5.Block;
 import org.apache.tapestry5.annotations.Import;
 import org.apache.tapestry5.annotations.Parameter;
 import org.apache.tapestry5.annotations.Property;
@@ -24,15 +23,5 @@ public class Layout
     @SuppressWarnings("unused")
     @Property
     private String pageName;
-
-    @SuppressWarnings("unused")
-    @Property
-    @Parameter(defaultPrefix = BindingConstants.LITERAL)
-    private String sidebarTitle;
-
-    @SuppressWarnings("unused")
-    @Property
-    @Parameter(defaultPrefix = BindingConstants.LITERAL)
-    private Block sidebar;
     
 }
